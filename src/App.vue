957b550d0fe8b5c3480eea100dda879b6b47d828
chetvergov <chetvergov@itopcase.ru>
2022-11-22 15:23:41 +0300

    ggfjhgj

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@
     </select>
     <input v-model="filterOptionValue"/>
     <button @click="filterData">Find</button>
+    <button @click="showModal = true">Add User</button>
     <div v-if="isLoading" class="spinner">
       <VueSpinnerIos color="black" size="100px"/>
     </div>
@@ -16,7 +17,7 @@
         </tr>
       </thead>
       <tbody>
-        <tr v-for="user in sortedAndFilteredTable" :key="user.email">
+        <tr v-for="user in sortedAndFilteredTable" :key="user.email" @click="setActiveRow(user)">
           <td v-for="value in columns" :key="value.email">{{user[value]}}</td>
         </tr>
       </tbody>
@@ -25,15 +26,41 @@
       <div :class="{active: activePage === page}" class="page" v-for="page in tablePages" :key="page" @click="getUsersData(page)">{{page}}</div>
       <string class="page" @click="getNextPages" v-show="tablePages < 10">Далее</string>
     </div>
+    <p v-if="activeUserRow.length">Doubleclick to delete item</p>
+    <table v-if="activeUserRow.length">
+      <thead>
+        <tr>
+          <th v-for="(column, index) in columns" :key="index">{{column}}</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="user in activeUserRow" :key="user.email" @dblclick ="deleteActiveUser(user)">
+          <td v-for="value in columns" :key="value.email">{{user[value]}}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
+  <ModalWindow v-model:showModal="showModal">
+    <form class="user-form" @submit.prevent>
+      <input v-model="newUser.firstName" placeholder="first name"/>
+      <input v-model="newUser.lastName" placeholder="last name"/>
+      <input v-model="newUser.email" placeholder="email" type="email"/>
+      <input v-model="newUser.phone" placeholder="phone"/>
+      <input v-model="newUser.address" placeholder="address"/>
+      <input v-model="newUser.description" placeholder="description"/>
+      <button @click="addNewUser">Add user</button>
+    </form>
+  </ModalWindow>
 </template>
 
 <script>
 import { VueSpinnerIos } from 'vue3-spinners'
+import _ from 'lodash'
+import ModalWindow from './components/ModalWindow.vue'
 
 export default {
   components: {
-    VueSpinnerIos
+    VueSpinnerIos, ModalWindow
   },
   name: 'App',
   data () {
@@ -48,7 +75,18 @@ export default {
       activePage: 1,
       rows: 5,
       tablePages: 5,
-      isLoading: false
+      isLoading: false,
+      activeUserRow: [],
+      showModal: false,
+      newUser: {
+        id: 1,
+        firstName: '',
+        lastName: '',
+        email: '',
+        phone: '',
+        address: '',
+        description: ''
+      }
     }
   },
   methods: {
@@ -86,6 +124,18 @@ export default {
       this.tablePages += 1
       this.activePage = this.tablePages
       this.getUsersData()
+    },
+    setActiveRow (user) {
+      this.activeUserRow.push(user)
+      this.activeUserRow = _.uniqBy(this.activeUserRow, 'email')
+    },
+    deleteActiveUser (user) {
+      this.activeUserRow = this.activeUserRow.filter(pers => pers.email !== user.email)
+    },
+    addNewUser () {
+      const newUser = { ...this.newUser }
+      this.users.unshift(newUser)
+      this.showModal = false
     }
   },
   mounted () {
@@ -112,7 +162,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 .container {
   width: 1280px;
   margin: 0 auto;
@@ -120,14 +170,14 @@ export default {
 
 table,
 td {
-    border: 1px solid #333;
-    padding: 5px;
+  border: 1px solid #333;
+  padding: 5px;
 }
 
 thead,
 tfoot {
-    background-color: #333;
-    color: #fff;
+  background-color: #333;
+  color: #fff;
 }
 
 th,
@@ -137,6 +187,10 @@ td {
 
 th {
   cursor: pointer;
+}
+
+tr {
+  position: relative;
 }
 
 .page-container {
@@ -159,6 +213,23 @@ th {
 .active {
   transform: scale(1.1);
   font-weight: bold;
+}
+
+.close {
+  position: absolute;
+  top: 0;
+  right: -30px;
+  cursor: pointer;
+  color: black;
+}
+
+.user-form {
+  border: 1px solid green;
+  border-radius: 5px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  padding: 15px;
 }
 
 </style>
---
+++ src/components/ModalWindow.vue
@@ -0,0 +1,38 @@
+<template>
+  <div v-show="showModal" class="modal-overlay" @click="hideModal">
+    <div @click.stop class="modal-content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    showModal: {
+      type: Boolean,
+      default: false
+    }
+  },
+  methods: {
+    hideModal () {
+      this.$emit('update:showModal', false)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.modal-overlay {
+  position: fixed;
+  inset: 0;
+  background-color: rgba(0, 0, 0, .5);
+  display: flex;
+}
+
+.modal-content {
+  margin: auto;
+  background-color: rgba(255, 255, 255, 1);
+  border-radius: 5px;
+}
+</style>
